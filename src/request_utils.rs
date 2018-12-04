9cdf45791fb4641dfb7fca07eb9facfd84f4baad
Serge Barinov <bool.rus@gmail.com>
2018-12-04 19:07:26 +0300

    получение torrent-файлов по сети

--- src/request_utils.rs
+++ src/request_utils.rs
@@ -1,18 +1,37 @@
-use futures_fs::{FsPool};
+
 use futures::{Future, Stream};
 use futures::future;
-use actix_web::HttpRequest;
+use actix_web::{HttpRequest, HttpMessage};
 use bytes::*;
 use actix_web::error;
+use actix_web::multipart::MultipartItem;
 
-pub fn invoke_file(_req: &HttpRequest) -> impl Future<Item=Bytes, Error=actix_web::Error> {
+/**
+  почему-то в IDE не работает автокомплит после collect
+  TODO: убрать после исправления https://github.com/intellij-rust/intellij-rust/issues/3111
+*/
+fn collect<S: Stream>(s: S) -> impl Future<Item=Vec<S::Item>, Error=S::Error> {
+    s.collect()
+}
 
-    let initial_size = 128*1024usize;
-    FsPool::default().read("/Users/bool/Downloads/alice.torrent",Default::default())
-        .map_err(error::ErrorInternalServerError)
-        .fold(BytesMut::with_capacity(initial_size),|mut buf,chunk| {
-            buf.put(chunk);
-            future::ok::<BytesMut, actix_web::Error>(buf)
+pub fn invoke_file(req: &HttpRequest) -> impl Future<Item=Bytes, Error=actix_web::Error> {
+    let stream = req.multipart().from_err()
+        .filter_map(|item| match item {
+            MultipartItem::Field(f) => Some(f),
+            MultipartItem::Nested(_) => None,
         })
-        .map(|buf|buf.into())
+        .and_then(|field| {
+            field.fold(BytesMut::with_capacity(512*1024), |mut buf, chunk| {
+                buf.put(chunk);
+                future::ok::<BytesMut,error::MultipartError>(buf)
+            })
+        })
+        .map(|b| Bytes::from(b))
+        .from_err();
+    collect(stream).map(|v| { //как еще получить владение на первый элемент вектора?
+        for b in v {
+            return b;
+        }
+        unreachable!()
+    })
 }
